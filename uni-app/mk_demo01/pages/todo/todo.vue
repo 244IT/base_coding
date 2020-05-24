29cf665e5119830b83dbb93a9e85a87e725c31c2
chh <635062564@qq.com>
2020-05-24 08:55:15 +0800

    todo

--- uni-app/mk_demo01/pages/todo/todo.vue
+++ uni-app/mk_demo01/pages/todo/todo.vue
@@ -1,44 +1,50 @@
 <template>
 	<view class="content">
-		<view class="todo-header" v-if="list.length!==0">
-			<!-- 状态栏的左侧 -->
-			<view class="header-left">
-				<text class="active-text">全部</text>
-				<text>10条</text>
+		<!-- 头部 -->
+		<view class="todo-header" v-if="list.length !== 0">
+			<view class="todo-header-left">
+				<text class="active-text">{{text}}</text>
+				<text>{{dateList.length}}</text>
 			</view>
-			<!-- 状态栏的右侧 -->
-			<view class="header-right">
-				<text class="header-right-item active-tab">全部</text>
-				<text class="header-right-item">待办</text>
-				<text class="header-right-item">已完成</text>
-			</view>
-		</view>
-		<!-- 缺省页 -->
-		<view class="default-img" v-if="list.length===0">
-			<view class="show-img">
-				<image src="../../static/default.png"></image>
+			<view class="todo-header-right">
+				<view class="todo-header-right-item" :class="{'active-tab': tabIndex === 1}" @click="tab(1)">全部</view>
+				<view class="todo-header-right-item" :class="{'active-tab': tabIndex === 2}" @click="tab(2)">待办</view>
+				<view class="todo-header-right-item" :class="{'active-tab': tabIndex === 3}" @click="tab(3)">已完成</view>
 			</view>
 		</view>
-		<view class="todo-content" v-for="item in list" :key="index" v-else>
-			<!-- 列表项 -->
-			<view class="todo-list todo-finish">
-				<view class="todo-list-checkbox">
+		<!-- 内容列表部分 -->
+		<view class="todo-content" v-if="list.length !== 0">
+			<view class="todo-list" 
+				  v-for="(item, index) in dateList" 
+				  :class="{'todo-finish': item.checked}" 
+				  :key="index" 
+				  @click="finish(item.id)">
+				<view class="todo-list_checkbox">
 					<view class="checkbox"></view>
 				</view>
-				<view class="todo-list-content">{{item.content}}</view>
+				<view class="todo-list_content">{{item.content}}</view>
 			</view>
-			
+		</view>
+		<!-- 缺省页面 -->
+		<view v-else class="default">
+			<view class="default-image">
+				<image src="../../static/default.png" mode="aspectFit"></image>
+			</view>
+			<view class="default-text">
+				<view class="default-text_item">您还没有创建待办事项</view>
+				<view class="default-text_item">点击下面+号创建一个吧</view>
+			</view>
 		</view>
 		<!-- 创建按钮 -->
 		<view class="create-todo" @click="create">
-			<text class="iconfont icon-jia"></text>
+			<view class="iconfont icon-jia" :class="{'create-todo-active': todoShow}"></view>
 		</view>
 		<!-- 输入框 -->
-		<view  class="create-content" >
+		<view class="create-content" v-if="active" :class="{'create-show': todoShow}">
 			<view class="create-content-box">
-				<!-- input 输入 -->
+				<!-- input框 -->
 				<view class="create-input">
-					<input type="text" v-model="value" placeholder="请输入您要创建的todo" />
+					<input type="text" placeholder="请输入你想输入的内容" v-model="value">
 				</view>
 				<!-- 发布按钮 -->
 				<view class="create-button" @click="add">
@@ -50,254 +56,346 @@
 </template>
 
 <script>
+		
 	export default{
+			
 		data() {
-			return{
-				value: '',
-				list: [
-					// {
-					// 	content: "test1"
-					// },
-					// {
-					// 	content: "test2"
-					// }
-				],
-				active: false
+			return {
+				list:[],
+				active: false,
+				value: "",
+				tabIndex: 1,
+				text: "全部",
+				todoShow: false
+			}
+		},
+		computed:{
+			dateList() {
+				let list = JSON.parse(JSON.stringify(this.list))
+				let newList = []
+				if(this.tabIndex === 1) {
+					this.text = "全部"
+					return list
+				}
+				
+				if(this.tabIndex === 2) {
+					this.text = "待办"
+					list.forEach((item) => {
+						if(!item.checked) {
+							newList.unshift(item)
+							
+						}
+					})
+					return newList
+				}
+				
+				if(this.tabIndex === 3) {
+					this.text = "已完成"
+					list.forEach((item) => {
+						if(item.checked) {
+							newList.unshift(item)
+							
+						}
+					})
+					return newList
+				}
 			}
 		},
 		methods:{
 			create() {
-				this.active = !this.active
+				if(this.active) {
+					this.close()
+				}else{
+					this.open()
+				}
+			},
+			// 开启动画
+			open() {
+				this.active = true
+				this.$nextTick(() => {
+					setTimeout(() => {
+						this.todoShow = true;
+					},50)
+				})
+			},
+			// 关闭动画
+			close() {
+				this.todoShow = false
+				this.$nextTick(() => {
+					setTimeout(() => {
+						this.active = false;
+					}, 350)
+				})
+			},
+			add() {
+				if(this.value === '') {
+					uni.showToast({
+						title: "请输入内容",
+						icon: "none"
+					});
+					return
+				}
+				this.list.unshift({
+					content: this.value,
+					id: new Date().getTime(),
+					checked: false
+				})
+				this.value = ''
+				this.close()
+			},
+			finish(id) {
+				console.log("被点击了")
+				console.log(id)
+				this.list.findIndex((item, index) => {
+					if(this.list[index].id === id) {
+						this.list[index].checked = !this.list[index].checked
+					}
+				})
+			},
+			tab(index) {
+				this.tabIndex = index
 			}
 		}
 	}
 </script>
 
 <style>
+	/* 引入字体图标 */
 	@import '../../common/icon.css';
 	
 	.todo-header{
+		height: 90rpx;
+		padding: 0 30rpx;
 		display: flex;
-		padding: 15px;
-		height: 45px;
 		align-items: center;
-		color: #333;
-		font-size: 12px;
+		font-size: 28rpx;
 		box-sizing: border-box;
 		box-shadow: -1px 1px 5px 0 rgba(0, 0, 0, 0.1);
+		background: #FFFFFF;
+		
 	}
-	.header-left{
+	.todo-header-left{
 		width: 100%;
 	}
-	.header-right{
+	.todo-header-right{
 		flex-shrink: 0;
+		display: flex;
+		
 	}
-	.header-right-item{
-		padding: 0 5px;
+	.todo-header-right-item{
+		padding: 0 10rpx;
+	}
+	/* 全部选中样式 */
+	.active-text{
+		font-size: 30rpx;
+		color: #279abf;
+		margin-right: 10rpx;
+	}
+	/* 头部右栏tab选中样式*/
+	.active-tab{
+		color: #279abf;
 	}
 	
-	.active-tab{
-		color: #279ABF;
-	}
-	.active-text{
-		font-size: 14px;
-		color: #279ABF;
-		padding-right: 5px;
-	}
-	/* 列表项的样式 */
+	/* 列表内容 */
 	.todo-content{
 		position: relative;
 	}
 	.todo-list{
+		position: relative;
 		display: flex;
 		align-items: center;
-		position: relative;
-		padding: 15px;
-		margin: 15px;
-		font-size: 12px;
-		box-shadow: -1px 1px 5px 1 rgba(0, 0, 0, 0.1),-1px 2px 1px 0 rgba(255, 255, 255) inset;
-		border-radius: 10px;
-		/* color: #fff; */
+		margin: 30rpx;
+		padding: 30rpx;
+		box-shadow: 2rpx 2rpx 10rpx 2rpx rgba(0, 0, 0, 0.1), -1px 2px 1px 0 rgba(255, 255, 255) inset;
+		font-size: 28rpx;
+		color: #0c3854;
 		background-color: #cfebfd;
+		border-radius: 20rpx;
+		
 		overflow: hidden;
 	}
-	/* 列表项的样式 */
-	.todo-list::after{
+	/* 列表左侧的竖条 */
+	.todo-list:after{
+		display: block;
 		content: "";
 		position: absolute;
 		top: 0;
 		left: 0;
 		bottom: 0;
-		width: 5px;
-		background-color: #91d1e8;
-	}
-	.todo-list-checkbox{
-		padding-right: 15px;
-	}
-	.checkbox{
-		width: 20px;
-		height: 20px;
-		border-radius: 50%;
-		background-color: #fff;
-		box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.1);
+		width: 10rpx;
+		background: #91d1e8;
 	}
 	
-	.todo-finish .checkbox{
-		background-color: #eee;
-		position: relative;
+	/* 列表内的圆 */
+	.todo-list_checkbox{
+		padding-right: 30rpx;
 	}
-	.todo-finish .checkbox::after{
+	
+	.checkbox{
+		width: 40rpx;
+		height: 40rpx;
+		border-radius: 50%;
+		background-color: #FFF;
+		box-shadow: 0 0 10rpx 4rpx rgba(0, 0, 0, 0.1);
+	}
+	
+	/* 完成情况下的选中球 */
+	.todo-finish .checkbox{
+		position: relative;
+		background: #eee;
+	}
+	.todo-finish .checkbox:after{
 		position: absolute;
-		content: "";
-		width: 10px;
-		height: 10px;
+		content: '';
+		width: 20rpx;
+		height: 20rpx;
 		top: 0;
-		right: 0;
 		left: 0;
+		right: 0;
 		bottom: 0;
 		margin: auto;
-		box-shadow: 0 0 2px 0 rgba(0,0,0,0.2) inset;
-		background-color: #CFEBFD;
 		border-radius: 50%;
+		background: #CFEBFD;
+		box-shadow: 0 0 2px 0px rgba(0, 0, 0, 0.2) inset;
 	}
-	.todo-finish .todo-list-content{
-		color: #999;
+	/* 完成情况下的内容 文字颜色和中划线 */
+		
+	.todo-finish .todo-list_content{
+		color: #999999;
 	}
-	/* 删除线 */
+		
 	.todo-finish.todo-list::before{
 		position: absolute;
-		content: "";
+		content: '';
 		top: 0;
 		bottom: 0;
-		left: 40px;
-		right: 10px;
-		height: 2px;
+		left: 80rpx;
+		right: 20rpx;
+		height: 4rpx;
 		margin: auto 0;
-		background: #BDCBB8;
+		background-color: #bdcdd8;
+		/* z-index: 1; */
 	}
-	..todo-finish.todo-list::after{
-		color: #ccc;
+	/* 完成情况下的左竖线颜色 */
+	.todo-finish.todo-list:after {
+		background: #cccccc;
 	}
 	
 	/* 创建按钮 */
-	.create-todo {
+	.create-todo{
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		position: fixed;
-		bottom: 20px;
+		bottom: 40rpx;
 		left: 0;
 		right: 0;
 		margin: 0 auto;
-		width: 50px;
-		height: 50px;
+		width: 100rpx;
+		height: 100rpx;
 		border-radius: 50%;
-		background-color: #deeff5;
-		box-shadow: -1px 1px 5px 2px rgba(0, 0, 0, 0.1), -1px 1px 1px 0 rgba(255, 255, 255) inset;
+		background-color:#deeff5;
+		box-shadow: -2rpx 2rpx 10rpx 4rpx rgba(0, 0, 0, 0.2), 
+					-2rpx 2rpx 2rpx 0 rgba(255, 255, 255) inset;
 	}
-	
-	.icon-jia {
-		font-size: 30px;
+		
+	.icon-jia{
+		font-size: 60rpx;
 		color: #add8e6;
 	}
-	
-	.create-content {
+	/* 创建内容 */
+		
+	.create-content{
 		position: fixed;
-		bottom: 95px;
-		left: 20px;
-		right: 20px;
+		bottom: 190rpx;
+		left: 40rpx;
+		right: 40rpx;
 		transition: all 0.3s;
 		opacity: 0;
-		transform: scale(0) translateY(200%)
+		transform: scale(0) translateY(200%);
 	}
-	
-	.create--show {
+	.create-show{
 		opacity: 1;
 		transform: scale(1) translateY(0);
 	}
 	
-	.create-content-box {
+	.create-content-box{
 		display: flex;
 		align-items: center;
-		padding: 0 15px;
-		padding-right: 0;
-		border-radius: 50px;
-		background: #DEEFF5;
-		box-shadow: -1px 1px 5px 2px rgba(0, 0, 0, 0.1), -1px 1px 1px 0 rgba(255, 255, 255) inset;
+		padding-left: 30rpx;
+		padding-left: 30rpx;
+		border-radius: 100rpx;
+		box-shadow: -2rpx 2rpx 10rpx 4rpx rgba(0, 0, 0, 0.1), -2rpx 2rpx 2rpx 0 rgba(255, 255, 255) inset;
+		background-color:#deeff5;
 		z-index: 2;
 	}
 	
-	.create-input {
+	.create-input{
 		width: 100%;
-		padding-right: 15px;
-		color: #add8e6;
+		padding-right: 30rpx;
+		color: #ADD8E6;
 	}
-	
-	.create-button {
+	/* 右边的创建按钮 */
+	.create-button{
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		flex-shrink: 0;
-		width: 80px;
-		height: 50px;
-		border-radius: 50px;
-		font-size: 16px;
+		height: 100rpx;
+		width: 160rpx;
+		border-radius: 50rpx;
+		font-size: 32rpx;
 		color: #88d4ec;
-		box-shadow: -2px 0px 2px 1px rgba(0, 0, 0, 0.1);
+		box-shadow: -2rpx 0 4rpx 2rpx rgba(0, 0, 0, 0.1);
 	}
-	
-	.create-content:after {
-		content: '';
+	/* 内容中间下箭头 */
+	.create-content::after{
 		position: absolute;
+		content: '';
+		width: 40rpx;
+		height: 40rpx;
 		right: 0;
 		left: 0;
-		bottom: -8px;
+		bottom: -16rpx;
 		margin: 0 auto;
-		width: 20px;
-		height: 20px;
-		background: #DEEFF5;
+		background-color:#deeff5;
 		transform: rotate(45deg);
-		box-shadow: 1px 1px 5px 2px rgba(0, 0, 0, 0.1);
+		box-shadow: 2rpx 2rpx 10rpx 4rpx rgba(0, 0, 0, 0.1);
 		z-index: -1;
 	}
-	
-	.create-content-box:after {
-		content: '';
+	/* create-content-box覆盖 */
+	.create-content-box::after{
 		position: absolute;
+		content: '';
+		width: 40rpx;
+		height: 40rpx;
 		right: 0;
 		left: 0;
-		bottom: -8px;
+		bottom: -16rpx;
 		margin: 0 auto;
-		width: 20px;
-		height: 20px;
-		background: #DEEFF5;
+		background-color:#deeff5;
 		transform: rotate(45deg);
 	}
-	
-	.default {
-		padding-top: 100px;
+	/* 缺省页面 */
+	.default{
+		padding-top: 200rpx;
 	}
-	
-	.image-default {
+	.default-image{
 		display: flex;
 		justify-content: center;
 	}
 	
-	.image-default image {
-		width: 100%;
-	}
-	
-	.default-info {
+	.default-text{
 		text-align: center;
-		font-size: 14px;
-		color: #CCCCCC;
+		color: #CCC;
+		font-size: 32rpx;
 	}
 	
+	/* 底部动画 */
 	.icon-jia {
 		transition: transform 0.3s;
 	}
-	
-	.create-todo-active {
+	.create-todo-active{
 		transform: rotate(135deg);
 	}
 </style>
